7b1974beaaecbf1f0a498c3ce2065dbba46dca4e
Barada Sahu <barry@native5.com>
2013-08-20 15:53:25 +0530

    Corrected Cache Path constant checl

--- src/Native5/UI/TemplatingEngine.php
+++ src/Native5/UI/TemplatingEngine.php
@@ -68,7 +68,7 @@ class TemplatingEngine
             $templates_path = 'views'.'/'.$templates_path;
         }
         // Configure renderer
-        $cache_path = defined(CACHE_PATH) ? CACHE_PATH : "cache";
+        $cache_path = defined('CACHE_PATH') ? CACHE_PATH : "cache";
         $this->_renderer = new \Twig_Environment(
             new \Twig_Loader_Filesystem($templates_path, 
             array(
