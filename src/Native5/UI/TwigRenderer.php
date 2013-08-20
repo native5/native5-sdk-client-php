6e79f8c40c26384073016c5a98b98f19811be9d7
Barada Sahu <barry@native5.com>
2013-08-20 16:15:40 +0530

    Fixed path issues with renderer

--- src/Native5/UI/TwigRenderer.php
+++ src/Native5/UI/TwigRenderer.php
@@ -84,9 +84,9 @@ class TwigRenderer implements Renderer
         global $app;
         $session = $app->getSessionManager()->getActiveSession();
         $category = $session->getAttribute('category').'/';
-        $staticResourcesPath = $app->getConfiguration()->getApplicationContext().'/public/'.$category;
+        $staticResourcesPath = '/'.$app->getConfiguration()->getApplicationContext().'/public/'.$category;
         if ($app->getConfiguration()->isLocal()) {
-            $staticResourcesPath = '/views/resources/'.$category;
+            $staticResourcesPath = '/'.$app->getConfiguration()->getApplicationContext().'/views/resources/'.$category;
         }
         $in_data = array (
             'items'          => $data,
